96802409845f512488db49c72bf1c5e7b42936bb
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-08-11 20:53:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let bod = document.getElementById('body');
+
+// function toggleHide() {
+//     let btn = document.getElementById('close');
+
+//     if (bod.style.display != "none") {
+//         bod.style.display = "none";
+//     } else {
+//         bod.style.display = "block";
+//     }
+// }
